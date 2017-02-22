958c205b4b9fca4677adddf710bdcec2b7ecb224
mungaiandela <jailbre3k@gmail.com>
2017-02-22 12:19:50 +0300

    [Chore] Add tests to assert users are created and sent emails

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.core import mail
-from django.test import TestCase
+from hc.test import BaseTestCase
 from hc.api.models import Check
 
 
-class LoginTestCase(TestCase):
+class LoginTestCase(BaseTestCase):
 
     def test_it_sends_link(self):
         check = Check()
@@ -19,19 +19,27 @@ class LoginTestCase(TestCase):
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
 
-        ### Assert that a user was created
+        # Assert that a user was created
+        users = [usr.username for usr in User.objects.all()]
+        self.assertIn('alice', users)
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
-        ### Assert contents of the email body
 
-        ### Assert that check is associated with the new user
+        # Assert contents of the email body
+        self.assertIn("Hello,\n\nTo log into healthchecks.io",
+                      mail.outbox[0].body)
+        # Assert that check is associated with the new user
+
+    def check_associate_new_user(self):
+        check = Check(name="mycheck", user='alice')
+        check.save()
+        self.assertEqual(check.user, 'alice')
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
         self.client.get("/accounts/login/")
         assert "bad_link" not in self.client.session
 
-        ### Any other tests?
-
+        # Any other tests?
