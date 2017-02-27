c77c712180668a34391e9f11cc0d428698e944e8
mungaiandela <jailbre3k@gmail.com>
2017-02-27 15:07:00 +0300

    [Chore] Change email backend to smtp

--- hc/settings.py
+++ hc/settings.py
@@ -136,7 +136,7 @@ STATICFILES_FINDERS = (
 )
 COMPRESS_OFFLINE = True
 
-EMAIL_BACKEND = "djmail.backends.default.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
