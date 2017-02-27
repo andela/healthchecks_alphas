3a3802eab7fc9b8e36698e3a6e29ac40d053aa3d
mungaiandela <jailbre3k@gmail.com>
2017-02-27 14:04:24 +0300

    [Chore] Fix email configuration

--- hc/settings.py
+++ hc/settings.py
@@ -141,7 +141,7 @@ EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = "HealthChecks <healthchecks.io>"
 
 
