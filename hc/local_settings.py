f93a16fcde754e1778fe082081606693a6bd00dd
Martin Mungai <martin.mungai@andela.com>
2017-02-28 11:50:13 +0300

    [Chore] update local settings

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -6,10 +6,3 @@ DATABASES = {
         'TEST': {'CHARSET': 'UTF8'}
     }
 }
-import os
-DJMAIL_REAL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
