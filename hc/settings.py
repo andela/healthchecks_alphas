2aeb3fc118a1cee2996dae402570d51c901cd9bf
Martin Mungai <martin.mungai@andela.com>
2017-02-28 11:51:36 +0300

    [Chore] Update settings.py

--- hc/settings.py
+++ hc/settings.py
@@ -136,13 +136,6 @@ STATICFILES_FINDERS = (
 )
 COMPRESS_OFFLINE = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
-ADMINS = [('HealthChecks', EMAIL_HOST_USER)]
-
 # Slack integration -- override these in local_settings
 SLACK_CLIENT_ID = None
 SLACK_CLIENT_SECRET = None
