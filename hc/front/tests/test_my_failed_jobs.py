0575e310ba8c4a441158328290bcfcfdd4fbb489
Lawrence Wachira <lawrence.wachira@andela.com>
2017-03-13 12:19:08 +0300

    [Chore] Add failed jobs test setup

---
+++ hc/front/tests/test_my_failed_jobs.py
@@ -0,0 +1,14 @@
+from hc.api.models import Check
+from hc.test import BaseTestCase
+from datetime import timedelta as td
+from django.utils import timezone
+
+
+class MyChecksTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(MyChecksTestCase, self).setUp()
+        self.check = Check(user=self.alice, name="Alice Was Here")
+        self.check.last_ping = timezone.now() - td(days=3)
+        self.check.status = "up"
+        self.check.save()
