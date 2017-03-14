4f418e4ec734491336fbb685cdb4ec614e95e06d
Daniel Wangai <daniel.maina@andela.com>
2017-03-14 20:04:01 +0300

    [Chore #139551665)] Add test for verbose plural

--- hc/blog/tests.py
+++ hc/blog/tests.py
@@ -6,3 +6,6 @@ class PostTestCase(TestCase):
     def test_string_representation(self):
         post = Post(title="Testing python apps using functional and unit testing")
         self.assertEqual(str(post), post.title)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Post._meta.verbose_name_plural), "posts")
