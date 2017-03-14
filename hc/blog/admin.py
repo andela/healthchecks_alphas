e487ea8d63357eb1cc306452e8e869a36cc6e55e
Daniel Wangai <daniel.maina@andela.com>
2017-03-14 19:33:31 +0300

    [Chore #139551665)] Add blog to admin interface

--- hc/blog/admin.py
+++ hc/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at', 'updated_at')
+    list_filter = ('author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    raw_id_fields = ('author',)
+
+
+admin.site.register(Post, PostAdmin)
