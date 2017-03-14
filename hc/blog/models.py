0e1206e594475cd8b05cf1bcaa00a4ac48c4bd82
Daniel Wangai <daniel.maina@andela.com>
2017-03-14 18:31:28 +0300

    [Feature #139551665)] Add blog model

--- hc/blog/models.py
+++ hc/blog/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    '''
+        fields for Post field
+    '''
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    user = models.ForeignKey(User, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        '''
+            Return a human readable representation of model instance
+        '''
+        return self.title
