75c487100fea97e9298cab67de8595f4d411cf4c
Daniel Wangai <daniel.maina@andela.com>
2017-03-14 19:58:06 +0300

    [Chore #139551665)] Add test for string representation of post object

--- hc/blog/tests.py
+++ hc/blog/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+class PostTestCase(TestCase):
+    def test_string_representation(self):
+        post = Post(title="Testing python apps using functional and unit testing")
+        self.assertEqual(str(post), post.title)
