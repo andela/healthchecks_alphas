212fea307ad47b691e4644213b1f3fa1f221f7cc
Daniel Wangai <daniel.maina@andela.com>
2017-03-15 10:40:57 +0300

    [Chore #139551665)] Refactor url for blog

--- hc/urls.py
+++ hc/urls.py
@@ -8,7 +8,7 @@ from hc.api.management.commands.sendreports import Command
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('hc.accounts.urls')),
-    url(r'^blog/', include('hc.blog.urls')),
+    url(r'^', include('hc.blog.urls')),
     url(r'^', include('hc.api.urls')),
     url(r'^', include('hc.front.urls')),
     url(r'^', include('hc.payments.urls'))
