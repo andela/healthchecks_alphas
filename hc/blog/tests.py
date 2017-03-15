f363f80229a04c86bf2335d82b5893bf045c6c42
Daniel Wangai <daniel.maina@andela.com>
2017-03-15 12:08:38 +0300

    [Chore #139551665)] Add test to test if response contains post

--- hc/blog/tests.py
+++ hc/blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from .models import Post
 
@@ -11,6 +12,18 @@ class PostTestCase(TestCase):
         self.assertEqual(str(Post._meta.verbose_name_plural), "posts")
 
 class BlogTestCases(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username="anonymous")
+    '''
+        Test that blog entries show up on post list page
+    '''
     def test_blog_page(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
+
+    def test_one_entry(self):
+        Post.objects.create(title="TIA", content="TIA content", author=self.user)
+        response = self.client.get("/blog/")
+        self.assertContains(response, 'TIA')
+        self.assertContains(response, 'TIA content')
