5eaefc6c124cd59c4db653471164432f0a602e18
mungaiandela <martin.mungai@andela.com>
2017-03-15 11:39:25 +0300

    [Chore] Add faqs url to healthchecks urls

--- hc/front/urls.py
+++ hc/front/urls.py
@@ -41,4 +41,5 @@ urlpatterns = [
     url(r'^about/$', views.about, name="hc-about"),
     url(r'^privacy/$', views.privacy, name="hc-privacy"),
     url(r'^terms/$', views.terms, name="hc-terms"),
+    url(r'^faqs/$', views.faqs, name="hc-faqs"),
 ]
