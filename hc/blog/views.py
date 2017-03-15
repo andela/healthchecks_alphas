88e21d37cf3208bce34b540f23b2fe5e2dcc7b09
Daniel Wangai <daniel.maina@andela.com>
2017-03-15 12:27:31 +0300

    [Chore #139551665)] Rename blog file

--- hc/blog/views.py
+++ hc/blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('-created_at')
-    return render(request, "blog/post_list.html", {"posts": posts})
+    return render(request, "blog/blog.html", {"posts": posts})
