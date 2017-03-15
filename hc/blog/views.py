57ddae3307114c46ec2b16aa611b7ce3936ce88d
Daniel Wangai <daniel.maina@andela.com>
2017-03-15 12:10:56 +0300

    [Feature #139551665)] Can list posts

--- hc/blog/views.py
+++ hc/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def post_list(request):
-    return render(request, "blog/post_list.html")
+    posts = Post.objects.order_by('-created_at')
+    return render(request, "blog/post_list.html", {"posts": posts})
