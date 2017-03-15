fd5226720652d9d8d2ddc47c090a5cbad8415c64
Daniel Wangai <daniel.maina@andela.com>
2017-03-15 18:46:41 +0300

    [Chore #139551665)] Add method for string representation of comment class

--- hc/blog/models.py
+++ hc/blog/models.py
@@ -30,3 +30,6 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body
